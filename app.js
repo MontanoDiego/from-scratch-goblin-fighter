773450a40f9b90e5918b23921b725c18c3c17103
MontanoDiego <diego.e2.montano@gmail.com>
2022-11-08 12:27:16 -0800

    grabbed DOM elements, defined variable states

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ import { goblinRender } from './render-utils.js';
 
 
 /* Get DOM Elements */
-
-
+const killsEl = document.getElementById('amygdalas-slayed');
+const hunterVitEl = document.getElementById('vitality');
+const nameInputForm = document.querySelector('form');
+const nameInputBtn = document.getElementById('name-btn');
+const amygdalaListEl = document.querySelector('.amygdalas');
 
 /* State */
-
-
+let amygdalasSlain = 0;
+let hunterVitality = 0;
+let amygdalas = [
+    { id: 1, name: 'ORPHEUS', vit: 3 }
+    { id: 2, name: 'SISYPHUS', vit: 2 }
+];
+let idAggregator = 3; 
 
 /* Events */
-
 
 
 /* Display Functions */
@@ -19,3 +26,4 @@ import { goblinRender } from './render-utils.js';
 
 
 // (don't forget to call any display functions you want to run on page load!)
+
